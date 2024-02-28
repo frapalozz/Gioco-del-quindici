ebb61e7d0f42632e06353eab2e6918bdba85d7b0
Francesco Palozzi <palozzifrancesco@libero.it>
2024-02-28 16:24:09 +0100

    bug vittoria fixed

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ griglia = shuffle(griglia);
 array = grigliaFinale(base2, 4, 16);
 let posizione = 16;
 let mosse = 0;
+let finito = false;
 
 // Generazione griglia
 function shuffle(array) {
@@ -62,151 +63,158 @@ function grigliaFinale(list, elementi, fin) {
 
 
 function muovi(e) {
-    let position = e;
-    console.log(position);
-    let blocco1 = document.getElementById(position.toString()).children[0];
-    let blocco2 = document.querySelector(".bloccoVuoto");
+    if(!finito) {
+        let position = e;
+        console.log(position);
+        let blocco1 = document.getElementById(position.toString()).children[0];
+        let blocco2 = document.querySelector(".bloccoVuoto");
 
-    let riga;
-    if(position <= 4) {riga = 1}
-    if(position > 4 && position <= 8) {riga = 2;}
-    if(position > 8 && position <= 12) {riga = 3;}
-    if(position > 12 && position <= 16) {riga = 4;}
-    let colonna = 4;
-    for(let i = 0; i < colonna; i++) {
-        if(riga == 1) {
-            if(riga + i == position) {
-                colonna = i;
-                riga--;
-                break;
+        let riga;
+        if(position <= 4) {riga = 1}
+        if(position > 4 && position <= 8) {riga = 2;}
+        if(position > 8 && position <= 12) {riga = 3;}
+        if(position > 12 && position <= 16) {riga = 4;}
+        let colonna = 4;
+        for(let i = 0; i < colonna; i++) {
+            if(riga == 1) {
+                if(riga + i == position) {
+                    colonna = i;
+                    riga--;
+                    break;
+                }
+            }
+            if(riga == 2) {
+                if(5 + i == position) {
+                    colonna = i;
+                    riga--;
+                    break;
+                }
+            }
+            if(riga == 3) {
+                if(9 + i == position) {
+                    colonna = i;
+                    riga--;
+                    break;
+                }
+            }
+            if(riga == 4) {
+                if(13 + i == position) {
+                    colonna = i;
+                    riga--;
+                    break;
+                }
             }
         }
-        if(riga == 2) {
-            if(5 + i == position) {
-                colonna = i;
-                riga--;
-                break;
-            }
+
+        if( riga-1 != -1 && array[riga-1][colonna] == 16) {
+            console.log(1);
+            mosse++;
+
+            document.getElementById(posizione).append(blocco1);
+            document.getElementById(position).append(blocco2); 
+
+            posizione = position;
+
+            let x = array[riga][colonna];
+            array[riga][colonna] = array[riga-1][colonna];
+            array[riga-1][colonna] = x;
+
+            x = griglia2[riga][colonna];
+            griglia2[riga][colonna] = griglia2[riga-1][colonna];
+            griglia2[riga-1][colonna] = x;
+
+            console.log(checkWin());
+            
         }
-        if(riga == 3) {
-            if(9 + i == position) {
-                colonna = i;
-                riga--;
-                break;
-            }
+        else if(riga+1 != 4 && array[riga+1][colonna] == 16) {
+            console.log(2);
+            mosse++;
+
+            document.getElementById(posizione).append(blocco1);
+            document.getElementById(position).append(blocco2);
+
+            posizione = position;
+
+            let x = array[riga][colonna];
+            array[riga][colonna] = array[riga+1][colonna];
+            array[riga+1][colonna] = x;
+
+            x = griglia2[riga][colonna];
+            griglia2[riga][colonna] = griglia2[riga+1][colonna];
+            griglia2[riga+1][colonna] = x;
+
+            console.log(checkWin());
+            
         }
-        if(riga == 4) {
-            if(13 + i == position) {
-                colonna = i;
-                riga--;
-                break;
-            }
+        else if(colonna-1 != -1 && array[riga][colonna-1] == 16) {
+            console.log(3);
+            mosse++;
+
+            document.getElementById(posizione).append(blocco1);
+            document.getElementById(position).append(blocco2); 
+
+            posizione = position
+
+            let x = array[riga][colonna];
+            array[riga][colonna] = array[riga][colonna-1];
+            array[riga][colonna-1] = x;
+
+            x = griglia2[riga][colonna];
+            griglia2[riga][colonna] = griglia2[riga][colonna-1];
+            griglia2[riga][colonna-1] = x;
+
+            console.log(checkWin());
+            
+        }
+        else if(colonna+1 != 4 && array[riga][colonna+1] == 16) {
+            console.log(4);
+            mosse++;
+            
+            document.getElementById(posizione).append(blocco1);
+            document.getElementById(position).append(blocco2); 
+
+            posizione = position;
+
+            let x = array[riga][colonna];
+            array[riga][colonna] = array[riga][colonna+1];
+            array[riga][colonna+1] = x;
+
+            x = griglia2[riga][colonna];
+            griglia2[riga][colonna] = griglia2[riga][colonna+1];
+            griglia2[riga][colonna+1] = x;
+
+            console.log(checkWin());
+            
         }
     }
-
-    if( riga-1 != -1 && array[riga-1][colonna] == 16) {
-        console.log(1);
-        mosse++;
-
-        document.getElementById(posizione).append(blocco1);
-        document.getElementById(position).append(blocco2); 
-
-        posizione = position;
-
-        let x = array[riga][colonna];
-        array[riga][colonna] = array[riga-1][colonna];
-        array[riga-1][colonna] = x;
-
-        x = griglia2[riga][colonna];
-        griglia2[riga][colonna] = griglia2[riga-1][colonna];
-        griglia2[riga-1][colonna] = x;
-
-        console.log(checkWin());
-        
-    }
-    else if(riga+1 != 4 && array[riga+1][colonna] == 16) {
-        console.log(2);
-        mosse++;
-
-        document.getElementById(posizione).append(blocco1);
-        document.getElementById(position).append(blocco2);
-
-        posizione = position;
-
-        let x = array[riga][colonna];
-        array[riga][colonna] = array[riga+1][colonna];
-        array[riga+1][colonna] = x;
-
-        x = griglia2[riga][colonna];
-        griglia2[riga][colonna] = griglia2[riga+1][colonna];
-        griglia2[riga+1][colonna] = x;
-
-        console.log(checkWin());
-        
-    }
-    else if(colonna-1 != -1 && array[riga][colonna-1] == 16) {
-        console.log(3);
-        mosse++;
-
-        document.getElementById(posizione).append(blocco1);
-        document.getElementById(position).append(blocco2); 
-
-        posizione = position
-
-        let x = array[riga][colonna];
-        array[riga][colonna] = array[riga][colonna-1];
-        array[riga][colonna-1] = x;
-
-        x = griglia2[riga][colonna];
-        griglia2[riga][colonna] = griglia2[riga][colonna-1];
-        griglia2[riga][colonna-1] = x;
-
-        console.log(checkWin());
-        
-    }
-    else if(colonna+1 != 4 && array[riga][colonna+1] == 16) {
-        console.log(4);
-        mosse++;
-        
-        document.getElementById(posizione).append(blocco1);
-        document.getElementById(position).append(blocco2); 
-
-        posizione = position;
-
-        let x = array[riga][colonna];
-        array[riga][colonna] = array[riga][colonna+1];
-        array[riga][colonna+1] = x;
-
-        x = griglia2[riga][colonna];
-        griglia2[riga][colonna] = griglia2[riga][colonna+1];
-        griglia2[riga][colonna+1] = x;
-
-        console.log(checkWin());
-        
-    }
-    console.log(griglia2);
+    
 }
 
 function checkWin() {
-    let j = 0;
-    let k = 0;
     let x = 0;
-    for(let i = 0; i < base.length; i++) {
-        if(griglia2[j][k++] > x) {
-            x++;
-            if(k == 3) {
-                k = 0;
-                j++;
+    for(let i = 0; i < griglia2.length; i++) {
+
+        for(let j = 0; j < griglia2[i].length; j++) {
+            console.log(griglia2[i][j])
+
+            if(x == 15) {
+                document.getElementById('mossa').innerHTML = "Vittoria: " + mosse;
+                document.getElementById('mossa').style.color = "green"
+                finito = true;
+                return true;
             }
+
+            if(griglia2[i][j] == x+1) {
+                x++; 
+                continue;
+            }
+            else{
+                x = 0;
+                document.getElementById('mossa').innerHTML = "Mosse: " + mosse;
+                return false;
+            } 
         }
-        else if(i = 15 && griglia[j][k] == 0) {
-            document.getElementById('mossa').innerHTML= 'Vittoria\nMosse: ' + mosse;
-            return true;
-        }
-        else {
-            document.getElementById('mossa').innerHTML= 'Mossa: ' + mosse;
-            return false;
-        }
+
     }
 }
 
@@ -217,3 +225,4 @@ function checkWin() {
 creaGriglia(griglia);
 griglia2 = grigliaFinale(griglia, 4, 0);
 
+let grz = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
